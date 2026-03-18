eea7877bf7c54dd0237fb394848a66f72c0741e2
Bandita Das <140870941+BanditaDas@users.noreply.github.com>
2026-03-18 18:14:41 +0530

    responsive added
    Updated Product component styles for responsiveness and improved layout.

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,12 +3,12 @@ import Button from './Button'
 
 function Product({val, mover ,count}) {
   return (
-    <div className='w-full py-20 h-[23rem] text-white'>
-        <div onMouseEnter={()=>{mover(count)}} className='max-w-screen-xl mx-auto flex items-center justify-between'>
-            <h1 className='text-5xl font-semibold'>{val.title}</h1>
-            <div className="dets w-1/3">
-                <p className='mb-10 text-lg'>{val.desciption}</p>
-                <div className="flex items-center gap-5">
+    <div className='w-full py-20 h-[23rem] text-white max-lg:h-auto max-lg:py-[5vh] max-md:py-[4vh]'>
+        <div onMouseEnter={()=>{mover(count)}} className='max-w-screen-xl mx-auto flex items-center justify-between max-lg:flex-col max-lg:items-start max-lg:px-[4vw] max-md:px-[5vw] max-lg:gap-[3vh] max-md:gap-[2vh]'>
+            <h1 className='text-5xl font-semibold max-lg:text-[6vw] max-md:text-[8vw]'>{val.title}</h1>
+            <div className="dets w-1/3 max-lg:w-[70vw] max-md:w-full">
+                <p className='mb-10 text-lg max-lg:mb-[4vh] max-md:mb-[3vh] max-lg:text-[2.5vw] max-md:text-[4vw]'>{val.desciption}</p>
+                <div className="flex items-center gap-5 max-lg:gap-[3vw] max-md:gap-[4vw] max-md:flex-wrap">
                     {val.live && <Button title = "Live Website" />}
                     {val.case && <Button title = "Case Study" />}
                 </div>
